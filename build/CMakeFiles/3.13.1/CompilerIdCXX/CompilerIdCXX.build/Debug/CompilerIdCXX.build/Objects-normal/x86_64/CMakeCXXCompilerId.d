8076f72b20ac656f7c051d22ace37fe1b96821be
Adrian Sanchez <skatehumor@yahoo.com>
2019-06-15 12:09:23 -0400

    Removed build folder from repo

--- build/CMakeFiles/3.13.1/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/adr7000/zma/build/CMakeFiles/3.13.1/CompilerIdCXX/CMakeCXXCompilerId.cpp
--- build/zma.build/Debug/ALL_BUILD.build/Script-1E0D5677AF5E4EB5946543BA.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/adr7000/zma/build -f /Users/adr7000/zma/build/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
