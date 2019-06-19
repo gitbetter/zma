84f5175c848964909f3738a100115b3ffbdbbeb2
Adrian Sanchez <adrian.sanchez@PGOSX147.local>
2019-06-19 17:07:20 -0400

    Added new methods + new matrix4x4 structure

--- includes/zma.hpp
+++ includes/zma.hpp
@@ -18,7 +18,77 @@ typedef float Float;
 
 namespace zma {
     
+    /*====================
+            Misc.
+     =====================*/
+    
     static constexpr Float infinity = std::numeric_limits<Float>::infinity();
+    
+    inline Float lerp(Float t, Float v1, Float v2) {
+        return (1 - t) * v1 + t * v2;
+    }
+    
+    inline bool quadratic(Float a, Float b, Float c, Float* t0, Float* t1) {
+        double discrim (double)b * (double)b - 4 * (double)a * (double)c;
+        if (discrim < 0) return false;
+        double rootDiscrim = std::sqrt(discrim);
+        
+        double q;
+        if (b < 0) q = -.5 * (b - rootDiscrim);
+        else q = -.5 * (b + rootDiscrim);
+        *t0 = q / a;
+        *t1 = c / q;
+        if (*t0 > *t1) std::swap(*t0, *t1);
+        return true;
+    }
+    
+    /*====================
+            Matrices
+     =====================*/
+    
+    struct matrix4x4 {
+        Float m[4][4];
+        
+        matrix4x4() {
+            m[0][0] = m[1][1] = m[2][2] = m[3][3] = 1.f;
+            m[0][1] = m[0][2] = m[0][3] =
+            m[1][0] = m[1][2] = m[1][3] =
+            m[2][0] = m[2][1] = m[2][3] = 0.f;
+        }
+        
+        matrix4x4(Float mat[4][4]) {
+            std::copy(&m[0][0], &m[0][0] + 16, &mat[0][0]);
+        }
+        
+        matrix4x4(Float t00, Float t01, Float t02, Float t03,
+                  Float t10, Float t11, Float t12, Float t13,
+                  Float t20, Float t21, Float t22, Float t23,
+                  Float t30, Float t31, Float t32, Float t33) {
+            m[0][0] = t00; m[0][1] = t01; m[0][2] = t02; m[0][3] = t03;
+            m[1][0] = t10; m[1][1] = t11; m[1][2] = t12; m[1][3] = t13;
+            m[2][0] = t20; m[2][1] = t21; m[2][2] = t22; m[2][3] = t23;
+            m[3][0] = t30; m[3][1] = t31; m[3][2] = t32; m[3][3] = t33;
+        }
+        
+        bool operator==(const matrix4x4& m2) const {
+            for (int i = 0; i < 4; i++) {
+                for (int i = 0; i < 4; i++) {
+                    if (m[i][j] != m2.m[i][j]) return false;
+                }
+            }
+            return true;
+        }
+        bool operator!=(const matrix4x4& m2) const {
+            for (int i = 0; i < 4; i++) {
+                for (int i = 0; i < 4; i++) {
+                    if (m[i][j] != m2.m[i][j]) return true;
+                }
+            }
+            return false;
+        }
+        
+    }
+    
     
     /*====================
             Vectors
