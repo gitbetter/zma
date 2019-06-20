bd6a0e1e2f6cd9f52e391dc4c257e0db4e03cad1
Adrian Sanchez <adrian.sanchez@PGOSX147.local>
2019-06-20 08:27:31 -0400

    Finished matrix struct + added basic operations to transform class

--- includes/zma.hpp
+++ includes/zma.hpp
@@ -87,6 +87,84 @@ namespace zma {
             return false;
         }
         
+        matrix4x4 operator*(const matrix4x4& m2) const {
+            matrix4x4 r;
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    r.m[i][j] = m[i][0] * m2.m[0][j] +
+                                m[i][1] * m2.m[1][j] +
+                                m[i][2] * m2.m[2][j] +
+                                m[i][3] * m2.m[3][j];
+                }
+            }
+            return r;
+        }
+        
+        friend matrix4x4 transpose(const matrix4x4& m);
+        friend matrix4x4 inverse(const matrix4x4& m);
+    }
+    
+    matrix4x4 transponse(const matrix4x4& m) {
+        return matrix4x4(m.m[0][0], m.m[1][0], m.m[2][0], m.m[3][0],
+                         m.m[0][1], m.m[1][1], m.m[2][1], m.m[3][1],
+                         m.m[0][2], m.m[1][2], m.m[2][2], m.m[3][2],
+                         m.m[0][3], m.m[1][3], m.m[2][3], m.m[3][3]);
+    }
+    
+    matrix4x4 inverse(const matrix4x4& m) {
+        int indxc[4], indxr[4];
+        int ipiv[4] = {0, 0, 0, 0};
+        Float minv[4][4];
+        memcpy(minv, m.m, 4 * 4 * sizeof(Float));
+        for (int i = 0; i < 4; i++) {
+            int irow = 0, icol = 0;
+            Float big = 0.f;
+            // Choose pivot
+            for (int j = 0; j < 4; j++) {
+                if (ipiv[j] != 1) {
+                    for (int k = 0; k < 4; k++) {
+                        if (ipiv[k] == 0) {
+                            if (std::abs(minv[j][k]) >= big) {
+                                big = Float(std::abs(minv[j][k]));
+                                irow = j;
+                                icol = k;
+                            }
+                        } else if (ipiv[k] > 1)
+                            Error("Singular matrix in MatrixInvert");
+                    }
+                }
+            }
+            ++ipiv[icol];
+            // Swap rows _irow_ and _icol_ for pivot
+            if (irow != icol) {
+                for (int k = 0; k < 4; ++k) std::swap(minv[irow][k], minv[icol][k]);
+            }
+            indxr[i] = irow;
+            indxc[i] = icol;
+            if (minv[icol][icol] == 0.f) Error("Singular matrix in MatrixInvert");
+            
+            // Set $m[icol][icol]$ to one by scaling row _icol_ appropriately
+            Float pivinv = 1. / minv[icol][icol];
+            minv[icol][icol] = 1.;
+            for (int j = 0; j < 4; j++) minv[icol][j] *= pivinv;
+            
+            // Subtract this row from others to zero out their columns
+            for (int j = 0; j < 4; j++) {
+                if (j != icol) {
+                    Float save = minv[j][icol];
+                    minv[j][icol] = 0;
+                    for (int k = 0; k < 4; k++) minv[j][k] -= minv[icol][k] * save;
+                }
+            }
+        }
+        // Swap columns to reflect permutation
+        for (int j = 3; j >= 0; j--) {
+            if (indxr[j] != indxc[j]) {
+                for (int k = 0; k < 4; k++)
+                    std::swap(minv[k][indxr[j]], minv[k][indxc[j]]);
+            }
+        }
+        return matrix4x4(minv);
     }
     
     
@@ -1098,5 +1176,24 @@ namespace zma {
          Transforms
     =====================*/
         
-    
+    class transform {
+    public:
+        transform() { }
+        transform(const matrix4x4& m) : m(m), mInv(inverse(m)) { }
+        transform(const matrix4x4& m, const matrix4x4& mInv) : m(m), mInv(mInv) { }
+        transform(const Float mat[4][4]) {
+            m = matrix4x4(mat);
+            mInv = inverse(m);
+        }
+        
+        friend transform inverse(const transform& t) {
+            return transform(t.mInv, t.m);
+        }
+        friend transform transpose(const transform& t) {
+            return transform(transpose(t.m), transpose(t.mInv));
+        }
+        
+    private:
+        matrix4x4 m, mInv;
+    }
 };
